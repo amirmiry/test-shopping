c9684dbb09381c9afc196e7d6218ac269d931275
amir <saturnloop.ai@gmail.com>
2025-10-04 20:57:16 +0330

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,9 @@
-def Contribiute ():
+def Contribiute (name: string):
     if selling  == "cole":
         print("changes are applied  . .. ")
 
 def evenNums (number):
     return number % 2 == 0
 
-
+print(evenNums(23))
+Contribiute("cole")
