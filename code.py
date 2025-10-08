844c613349b0c3567f8f6391d86412fd3b0ef880
AmirHosein <Saturnloop.ai@gmail.com>
2025-10-08 22:27:01 +0330

    codding class Nums

--- code.py
+++ code.py
@@ -11,3 +11,12 @@ def recersive (n):
 print(evenNums(23))
 Contribiute("cole")
 print("Done")
+
+
+class Nums:
+    def __init__(self, number:float , name:str):
+        self.name = name
+        self.phone= number
+
+
+num1 = Nums(34  , "thirtyfour")
