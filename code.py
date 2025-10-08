e2e934a262e0f8cf44dedc3f5daeaf2b1809eeca
AmirHosein <Saturnloop.ai@gmail.com>
2025-10-08 22:47:15 +0330

    deleting the methods

--- code.py
+++ code.py
@@ -1,16 +1,3 @@
-def Contribiute (name: string):
-    if selling  == "cole":
-        print("changes are applied  . .. ")
-
-def evenNums (number):
-     return number % 2 == 0
-    
-def recersive (n):
-    for i in range (n):
-        return n+1
-print(evenNums(23))
-Contribiute("cole")
-print("Done")
 
 
 class Nums:
@@ -20,3 +7,4 @@ class Nums:
 
 
 num1 = Nums(34  , "thirtyfour")
+
